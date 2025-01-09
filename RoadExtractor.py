2e03fca39251ade9de04f24d3202196071e40269
Mohamed M. Hesham <mohamed_hesham203@icloud.com>
2025-01-09 04:43:27 +0200

    Update RoadExtractor.py

--- RoadExtractor.py
+++ RoadExtractor.py
@@ -46,8 +46,8 @@ class RoadExtractor:
     
     def save_model(model, name):
         model.save(f'trained_models/{name}.keras')
-    def load_model(name):
-        return ks.models.load_weights(f'trained_models/{name}.keras')
+    def load_model(model, name):
+        return model.load_weights(f'trained_models/{name}.keras')
     def get_predction(model, image , threshold=0.1):
         pred_image = model.predict(image)
         pred_image = pred_image[0]
