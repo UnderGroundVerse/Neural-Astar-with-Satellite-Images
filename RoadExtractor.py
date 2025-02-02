bec61ed1ec6228d26673794868b4ccfafc343beb
Mohamed M. Hesham <mohamed_hesham203@icloud.com>
2025-02-02 02:01:44 +0200

    Update RoadExtractor.py

--- RoadExtractor.py
+++ RoadExtractor.py
@@ -3,10 +3,11 @@ from keras import layers
 import numpy as np
 
 class RoadExtractor(ks.Model):
-    def __init__(self, input_shape=(256, 256, 3)):
+    def __init__(self, input_dim=(256, 256, 3)):
         super(RoadExtractor, self).__init__()
-        self.input_shape = input_shape
+        self.input_dim = input_dim
         self.rd_layers = [
+       layers.InputLayer(input_shape=self.input_dim),
        layers.Conv2D(32, (3, 3), strides=1 ,activation='relu', padding='same'),
        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
        layers.MaxPooling2D((2, 2), padding='same'),
@@ -45,10 +46,11 @@ class RoadExtractor(ks.Model):
       return x
 
     def build_model(self, optimizer='adam',loss='binary_crossentropy',metrics=['accuracy']):
-        inputs = ks.Input(shape=self.input_shape)
+        inputs = ks.Input(shape=self.input_dim)
         outputs = self.call(inputs)
         self.model = ks.Model(inputs=inputs,outputs=outputs)
         self.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+        self.build( (None, *self.input_dim))
     def save_model(self, name):
         self.save_weights(f'trained_models/{name}.h5')
     def load_model(self, name):
